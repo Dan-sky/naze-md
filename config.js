ca9bd875fc7461f17e853bb6e6bf3080475c70a1
Dan-sky <79742519+Dan-sky@users.noreply.github.com>
2022-10-01 15:31:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6289529737500']
+global.ownernomer = "6289529737500"
+global.premium = ['6289529737500']
+global.packname = 'Danzz Bot'
+global.author = 'ᴹᴿ᭄ ItsMeDanzzོ ×፝֟͜×'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
@@ -41,7 +41,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 50
 }
 global.thumb = fs.readFileSync('./image/naze.jpg')
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
